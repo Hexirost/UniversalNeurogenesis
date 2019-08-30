35a9294dd08c6102497a6a2523df00273a60330b
Hexirost <hexirost@gmail.com>
2019-08-30 15:41:30 -0400

    first commit

---
+++ Neuron.py
@@ -0,0 +1,17 @@
+class Neuron:
+    
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return data
+
+    def __str__(self):
+        return str(data)
+
+class SensoryNeuron(Neuron):
+
+    def __init__(self, sensory_input):
+        pass
+
+    
---
+++ exercise.py
@@ -0,0 +1,8 @@
+import gym
+import numpy as np
+import networkx
+from NeuronGraph import NeuronGraph
+
+env = gym.make("MsPacman-v0")
+
+brain = NeuronGraph()
